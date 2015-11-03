8333e2535f3fb40120068ec4236c58bce9c1d57a
Anton Vinogradov <av@apache.org>
2015-11-03 11:44:08 +0300

    1093

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/preloader/GridDhtPartitionDemander.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/preloader/GridDhtPartitionDemander.java
@@ -23,7 +23,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -145,7 +144,12 @@ public class GridDhtPartitionDemander {
      * Stop.
      */
     void stop() {
-        rebalanceFut.cancel();
+        try {
+            rebalanceFut.cancel();
+        }
+        catch (Exception ex) {
+            rebalanceFut.onDone(false);
+        }
 
         lastExchangeFut = null;
 
